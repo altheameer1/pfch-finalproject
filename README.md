88f91ba51f0bdab27078390eb1c26c800bf46195
altheameer1 <87437374+altheameer1@users.noreply.github.com>
2023-05-09 14:40:38 -0400

    Final commit before project submission. Added vital record match search and text file output.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # pfch-finalproject
+
+Using the program:
+
+- Enter an address that includes building number, street name, and borough. Keep in mind that many addresses are not going to be represented in the city directory dataset. Your best best is trying something in lower Manhattan or the Chelsea area.
+
+- You might have to wait a minute or two for the program to work. It takes a while to iterate through each entry in both datasets. 
+
+
+Project description:
+
+I used this program to begin building a new tool for genealogy and building research using two open-access datasets available from NYPL's Space/Time Directory and NYC Open Data. Currently, it is still very hard to simply search an NYC address and find a list of people who used to live there. Though many of NYPL'S New York City Directories are digitized and machine-readable data is available from the Space/Time Directory, there isn't currently a way for the public to search an address accross all directories at once. I wanted to start creating a tool that would eventually allow for this type of searching, while also connecting the directories dataset to historical NYC vital records available from the Municipal Archives, through NYC Open Data. This would allow users to see not only a list of the people at a certain address from the directory, but potential matches of important life events (birth, marriage, death) from the historical vital records.
+
+Output is displayed in a .txt file and shows each match, their name, occupation, year listed in the directory, and address information, as well as any vital records created in the years listed that match the first and last name of the entry.
+
+
+The general process of my work was:
+
+1. Getting to know the nyc-directories dataset, downloading it as CSV (the other file types were too large for my computer to process), and determinging that I wanted to match searched addresses to directory entires through coordinates. NYPL Labs staff had used their nyc-addresses tool to assign coordinates to the historical addresses in the directory entries based on their historical locations, meaning changed in house number/street name over time wouldn't affect the matches I found.
+
+2. Figuring out how to convert a user-inputted address into coordinates. I ended up using the geopy module to connect to the Nominatim geocoder API.
+
+3. Determing how to get user-generated coordinates to match the right ones in the directories dataset. I ended up rounding my coordinates to the fourth decimal point, which represesnts about 11.1m of distance, and letting matches occur if the rounded coordinates were within + or - 0.0001 of each other.
+
+4. Testing to see if random addresses I entered would return matches. I realized that the directories don't cover most of Brooklyn, and anywhere below 14th street in Manhattan or in Chelsea is the most likely to return matches, but there were still gaps in the data. This is probably due to the way that people were distributed across the city in the mid-to-late 1800s, and the fact that most of NYPL's digitized directories only cover Manhattan.
+
+5. Looping through the historical vital records dataset and finding matches by comparing first and last name and the year the vital record was created. I wanted to only find certificates from the year that that person was living (or working) at the address in the directory.
+
+Future ways to increase accuracy:
+
+Unfortunately, the historical vital records dataset currently does not contain information like home address or occupation that might help make my results a lot more accurate. However, in the future I'd like to increase the accuracy of the results by comparing the year the vital record was created, the type of record, and the year the person is listed in the city directory to weed out obvious false matches (i.e. if someone was born that year, they probably aren't listed in the city directory as being a blacksmith). This would also let me expand my search by looking at vital records that were created outside the exact years someone is listed in the directory, but could still correspond to the same individual.
+
+I also would like to eventually create a web-based search tool with a clean, accessible user interface for people to use.
+
+Data sources: 
+
+NYPL Space/Time Directory city-directories dataset: http://spacetime.nypl.org/#data
+NYC Open Data Historical Vital Records dataset: https://data.cityofnewyork.us/City-Government/New-York-City-Historical-Vital-Records-Digitized-C/xdc2-zgy3 
+
--- nyc_records.py
+++ nyc_records.py
@@ -1,39 +1,50 @@
 import csv
 import geopy
 from geopy.geocoders import Nominatim
+import ast
+import re
 
 
 # ----- Finding lat. and long. for a specific address (will be user-generated) -----
 
-"""
+ 
 
-address = "30 St James Pl, Brooklyn"
+# address = "241 W 29th St, Manhattan"
+address = input('What is your address? Please include the borough! (ex: "241 W 29th St, Manhattan")\n')
 
 locator = Nominatim(user_agent="myGeocoder")
 
-print("Finding the exact location of the address, this might take a minute or so...")
+print("Finding the exact location of the address...")
 
 location = locator.geocode(address, timeout=None)
 
-print("Exact location...well, located!")
+print()
 
-# NOTE: Coordinates in the city directories data are stored as lon., lat. while Nominatim stores them the opposite order
+coordinates = [round(location.longitude, 4), round(location.latitude, 4)]
 
 
-#Creating a dictionary to store each lat. and long. with its corresponding info
-place_matches = {}
+print("Exact location...well, located! Your coordinates are {}.\n".format(coordinates))
 
-# ----- Opening the directories and vital records data files and converting them to DictReader objects
 
-"""
+# ----- Opening the directory data and converting to a DictReader object
 
+
+
+
+# Creating lists to store info about matched addresses
 my_city_directory = []
 my_vital_records = []
 
-with open("city-directories.csv","r") as dir_file, open("vital-records.csv","r") as vital_file:
-	
+print()
+print("Searching the city directory data for entries that match your address...")
+print()
 
-	vital_reader = csv.DictReader(vital_file)
+
+# My laptop was taking way too long to load the NDJSON file, so I went with the CSV instead
+
+with open("city-directories.csv","r") as dir_file:
+
+
 	
 	dir_reader = csv.DictReader(dir_file)
 
@@ -41,60 +52,174 @@ with open("city-directories.csv","r") as dir_file, open("vital-records.csv","r")
 	# ----- POPULATING MY_CITY_DIRECTORY -----
 
 	# Going through each dictionary in the DictReader object of the city directory entries
+	# I only want to pull out the entries that have a matching(ish) latitude and longitude
+	# I'll round the latitude and longitude to 4 decmimal places, which represents about 11.1 meters
+	
 	# Adding the keys I need to a new list of dictionaries
 
 	my_keys = ["id", "name", "type", "validSince", "validUntil", "$.data.occupation", "$.data.locations", "$.data.geocoded", "geometry"]
 
+
+
+
 	for entry in dir_reader:
-		temp_dict = {}
-		for key in entry:
-			if key in my_keys:
 
-				# Splits name into a list of separate parts (last, first, middle)
+		# Some entries from the OCR'd images are ads, titles, etc. and don't have any address info.
+		# I want to filter those out so my code doesn't break
+
+		if entry["geometry"] != "":
+
+			# Converting the geometry value to a dictionary (it got flattened to a list in the CSV)
+			
+			geometry = ast.literal_eval(entry["geometry"])
+
+			
+			possible_longs = []
+			possible_lats = []
+
+			# "Point" entries have one address per name, while "MultiPoint" have more than one
+
+			if geometry["type"] == "Point":
 				
-				if key == "name":
-					newval = entry["name"].split(" ")
-					temp_dict[key] = newval
-				else: 
-					temp_dict[key] = entry[key]
+				# Adding the longitudes and latitudes to their own lists, as well as deviations in order to broaden the search radius
+				
+				possible_longs.extend([round(geometry["coordinates"][0], 4), round(geometry["coordinates"][0]+0.0001, 4), round(geometry["coordinates"][0]-0.0001, 4)])
+				possible_lats.extend([round(geometry["coordinates"][1], 4), round(geometry["coordinates"][1]+0.0001, 4), round(geometry["coordinates"][1]-0.0001, 4)])
 
-		my_city_directory.append(temp_dict)
+			elif geometry["type"] == "MultiPoint":
+				
+				# Adding the longitudes and latitudes to their own lists, but accounts for all items in the nested list
+				possible_longs.extend([round(geometry["coordinates"][0][0], 4), round(geometry["coordinates"][0][0]+0.0001, 4), round(geometry["coordinates"][0][0]-0.0001, 4), round(geometry["coordinates"][1][0], 4), round(geometry["coordinates"][1][0]+0.0001, 4), round(geometry["coordinates"][1][0]-0.0001, 4)])
+				possible_lats.extend([round(geometry["coordinates"][0][1], 4), round(geometry["coordinates"][0][1]+0.0001, 4), round(geometry["coordinates"][0][1]-0.0001, 4),round(geometry["coordinates"][1][1], 4), round(geometry["coordinates"][1][1]+0.0001, 4), round(geometry["coordinates"][1][1]-0.0001, 4)])
 
-	# ----- POPULATING MY_VITAL_RECORDS -----
-	# Not adding any entries where the first and last name are blank, as the name is all I have for linking
+			# If the coordinates are a match for the address inputted by the user, create a dictionary including the keys I'm interested in
+			# Then add the dictionary to my my_city_directory list
+
+			if ((coordinates[0] in possible_longs) and (coordinates[1] in possible_lats)):
+				temp_dict = {}
+				for key in entry:
+					
+					# Splits name into a list of separate parts (last, first, middle)
+					
+					if key == "name":
+						cleaned_name = entry["name"].strip(" .«")
+						newval = cleaned_name.split(" ")	
+						temp_dict[key] = newval
+			
+
+
+						
+					
+					elif key in my_keys:
+						temp_dict[key] = entry[key]
+				
+				# Filterning out entries for businesses - I only want real people so I can compare to vital records
+
+				if "Co" not in temp_dict["name"] and "&" not in temp_dict["name"] and "Brothers" not in temp_dict["name"]:
+					my_city_directory.append(temp_dict)
+
+
+
+
+for match in my_city_directory:
+
+	# Getting rid of random non-alpha characters and empty strings in the name list
 	
+	new_name = [] 
+
+	for name_piece in match["name"]:
+
+		if name_piece != " " and name_piece != ".":
+			name_piece = re.sub("«", "", name_piece)
+			new_name.append(name_piece)
+
+	match["name"] = new_name
+
+	match["certinfo"] = []
+
+
+	"""
+
+	print()
+	print("Name:", end=" ")
+	for name in match["name"]:
+		print(name + ", ", end=" ")
+	print()
+	print("Occupation: ",match["$.data.occupation"])
+	print("Valid since: " + match["validSince"]) 
+	print("Valid until: " + match["validUntil"])
+	print(match["$.data.locations"])
+	print(match["$.data.geocoded"])
+	print(match["geometry"])
+	
+	"""
+
+# ----- POPULATING MY_VITAL_RECORDS -----
+# Not adding any entries where the first and last name are blank, as the name is all I have for linking
+
+
+
+
+with open("vital-records.csv","r") as vital_file:
+
+	vital_reader = csv.DictReader(vital_file)
+
 	for cert in vital_reader:
-		if cert["First Name"] != "" or cert["Last Name"] != "":
-			my_vital_records.append(cert)
-
-# ----- Now I'm done dealing with the original source files -----
-
-# ----- Create a dictionary of matches in the city directories based on the inputted address
-
-test_coord = [40.68887475,-73.96522064999999]
-
-# Re-ordering the coordinates so they match the ones in the city directories data
-
-temp_coord1 = test_coord[0]
-temp_coord2 = test_coord[1]
-
-test_coord = [temp_coord2, temp_coord1]
-
-print(my_city_directory[3]["geometry"]["type"])
+		if (cert["First Name"] != "" and cert["Last Name"] != "") and int(cert["Certificate Year"])<1880:
+			for match in my_city_directory:
+				if (match["name"][0] == cert["Last Name"] and match["name"][1] == cert["First Name"]) and (int(cert["Certificate Year"]) >= int(match["validSince"]) and int(cert["Certificate Year"]) <= int(match["validUntil"])):
+					match["certinfo"].append(cert)
 
 
+print ("Done matching certificates!")
 
-"""
+address_file = str(re.sub("[^0-9a-zA-Z_]", "", address))
 
-for entry2 in my_city_directory:
-	if entry2["geometry"]["type"] == "Point":
-		if entry2["geometry"]["coordinates"] == test_coord:
-			print("found a match!")
-			print(entry2)
+with open("address_results"+address_file+".txt", "w") as outfile:
+	outfile.write("Thank you for searching " + address + ". Here are your results!")
+	outfile.write("\n")
+	outfile.write("\n")
 
-loc_matches = {}
+	counter = 1
 
-"""
+	for match in my_city_directory:
+		outfile.write("-------------------- Match #" + str(counter) + "-------------------------\n")
+		counter += 1
+		
+		outfile.write("Name: ")
+		
+		for name in match["name"]:
+			outfile.write(name + ", ")
+		outfile.write("\n")
+		outfile.write("Occupation: " + match["$.data.occupation"] + "\n")
+		outfile.write("Valid since: " + match["validSince"] + "\n") 
+		outfile.write("Valid until: " + match["validUntil"] + "\n")
+		outfile.write("Historical address data: " + match["$.data.locations"] + "\n")
+		outfile.write("Geocoded address data as of ~2018: " + match["$.data.geocoded"] + "\n")
+		outfile.write("\n")
+		outfile.write("Vital Record Matches: " + "\n")
+		outfile.write("\n")
+		
+		for certmatch in match["certinfo"]:
+			outfile.write("\n")
+			outfile.write("Name: " + certmatch["Last Name"] + " " + certmatch["First Name"] + "\n")
+			outfile.write("Certificate type: " + certmatch["Type"] + "\n")
+			outfile.write("County: " + certmatch["County"] + "\n")
+			outfile.write("Certificate number: " + certmatch["Certificate Number"] + "\n")
+			outfile.write("Age at death (if applicable): " + certmatch["Age at Death"] + "\n")
+			outfile.write("Certificate year: " + certmatch["Certificate Year"] + "\n")
+
+		outfile.write("\n")
+
+
+
+
+
+
+
+
+
+
 
 
 
